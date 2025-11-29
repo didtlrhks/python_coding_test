9d423d95ccc218cabe8a975955103718f127428b
sikwan <yysskk99999@naver.com>
2025-11-29 16:15:10 +0900

    ## add

--- 2178.py
+++ 2178.py
@@ -24,7 +24,7 @@ def BFS(x,y):
             if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
                 queue.append((nx,ny))
                 graph[nx][ny] = graph[x][y] + 1
-                
+                #갈수있는곳을 판단하고  하나씩 넣었다가 +1 하고 다시 뺌 그리고 그다음 갈 수 있는곳 분석 이런식으로 
     return graph[N-1][M-1]
 
 print(BFS(0,0))
