7207593c362e6a71446bb9573211defaf4aae607
sikwan <yysskk99999@naver.com>
2025-12-03 17:11:40 +0900

    BFS add

--- 1260(4).py
+++ 1260(4).py
@@ -4,14 +4,14 @@ N,M,V = map(int,input().split())
 graph = [[] for _ in range(N+1)]
 visit = [False] * (N+1)
 
-for i in range(1,N+1):
+for i in range(M):
     S,E = map(int,input().split())
     graph[S].append(E)
     graph[E].append(S)
     
     
 for i in range(1,N+1):
-    graph[i].sort
+    graph[i].sort()
 
 
 
