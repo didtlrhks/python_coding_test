e5adabb66aaa1c26c5172de8bce4ce2b471497b8
sikwan <yysskk99999@naver.com>
2025-12-05 22:18:17 +0900

    2606

---
+++ 2606.py
@@ -0,0 +1,31 @@
+import sys
+
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N+1)]
+visit = [False] * (N+1)
+count = 0
+
+
+for i in range(M):
+    S,E = map(int,input().split())
+    graph[S].append(E)
+    graph[E].append(S)
+    
+
+def DFS(n):
+     
+    visit[n] = True
+    print(count)
+    for i in graph[n]:
+       if not visit[i]:
+           count += 1
+           
+           DFS(i)
+
+
+DFS(N)     
+           
+    
+    
+    
