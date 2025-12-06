7eb86028626a8c6cd3d7db2b17f1ab93f3c446d2
sikwan <yysskk99999@naver.com>
2025-12-06 15:58:22 +0900

    2667 add

--- 2667.py
+++ 2667.py
@@ -4,13 +4,14 @@ from collections import deque
 N = int(input())
 graph = [list(input().strip()) for _ in range(N)]
 visit = [[False] * N for _ in range(N)]
+result = []
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 
 
-print(graph)
+
 
 def BFS(x,y):
     queue = deque()
@@ -23,6 +24,26 @@ def BFS(x,y):
         for dir in range(4):
             nx = x + dx[dir]
             ny = y + dy[dir]
+            
+            if 0 <= nx < N and 0 <= ny < N:
+                if not visit[nx][ny] and graph[nx][ny] == '1':
+                    visit[nx][ny] = True
+                    queue.append((nx,ny))
+                    count += 1
+                    
+    return count
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == '1' and not visit[i][j]:
+            result.append(BFS(i,j))
+            
+result.sort()
+
+print(len(result))
+for r in result:
+    print(r)
+
             
             
 
