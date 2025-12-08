a37aa8e2968fac75e955eb09e72dfc64e1f32f89
sikwan <yysskk99999@naver.com>
2025-12-08 22:43:30 +0900

    2667 err sol

--- 2667(1).py
+++ 2667(1).py
@@ -4,7 +4,7 @@ from collections import deque
 N = int(input())
 graph = [list(input().strip()) for _ in range(N)]
 visit = [[False] * N for _ in range(N)]
-dx = [-1.1,0,0]
+dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 result = []
@@ -12,7 +12,7 @@ result = []
 
 def BFS(x,y):
     queue = deque()
-    queue.append(x,y)
+    queue.append((x,y))
     visit[x][y] = True
     count = 1
     
@@ -23,10 +23,10 @@ def BFS(x,y):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if 0<= nx <=N and 0 <= ny <= N:
+            if 0<= nx <N and 0 <= ny < N:
                 if not visit[nx][ny] and graph[nx][ny] == '1':
                     visit[nx][ny] = True
-                    queue.append(nx,ny)
+                    queue.append((nx,ny))
                     count += 1
                     
     return count
@@ -34,7 +34,7 @@ def BFS(x,y):
                 
 for i in range(N):
     for j in range(N):
-        if graph[i][j] and not visit[i][j]:
+        if graph[i][j] == '1' and not visit[i][j]:
             result.append(BFS(i,j))
             
 result.sort()
