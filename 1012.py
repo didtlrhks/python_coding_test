810b5777a4193fc0eb4a093fab5ad9460e21457a
sikwan <yysskk99999@naver.com>
2025-12-11 19:29:46 +0900

    까비 문제

--- 1012.py
+++ 1012.py
@@ -2,64 +2,46 @@ import sys
 from collections import deque
 
 T = int(input())
-L = [-1,1,0,0]
-H = [0,0,-1,1]
-result = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-
-
+def BFS(x, y, graph, visit, N, M):
+    queue = deque()
+    queue.append((x, y))
+    visit[y][x] = True
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            
+            # 범위 체크
+            if 0 <= nx < M and 0 <= ny < N:
+                # 배추가 있고 방문 안 했으면
+                if not visit[ny][nx] and graph[ny][nx] == 1:
+                    visit[ny][nx] = True
+                    queue.append((nx, ny))
+    
+    return 1   # BFS 1번 실행 = 지렁이 1마리 필요
 
 for _ in range(T):
-    M,N,K = map(int,input().split())
-    visit = [[False] * M for _ in range(N)]
+    M, N, K = map(int, input().split())
+    
     graph = [[0] * M for _ in range(N)]
+    visit = [[False] * M for _ in range(N)]
+    
     for _ in range(K):
-        X,Y = map(int,input().split())
+        X, Y = map(int, input().split())
         graph[Y][X] = 1
         
-    result = 0
-      
-        
-        
-   
-
-    def BFS(x,y):
-        queue = deque()
-        queue.append((x,y))
-        visit[y][x] = True
-        count = 0
-        
-        while queue:
-            x,y = queue.popleft()
-            
-            
-            for i in range(4):
-                nx = x + L[i]
-                ny = y + H[i]
-            
-            
-                if 0 <= nx < M and 0 <= ny < N:
-                    if not visit[nx][ny] and graph[nx][ny] == 1:
-                        queue.append((nx,ny))
-                        visit[nx][ny] = True
-                
-                    
-        return 1
-
-
-    for i in range(N):
-        for j in range(M):
-            if 0 <= i < M and 0 <= j < N:
-                if not visit[i][j] and graph[i][j] == 1:
-                    result.append((BFS(i,j)))
-                    visit = True
-                    []
-
-                        
-            
-            
-        
-        
-
-        
-        
+    worm_count = 0
+    
+    for y in range(N):
+        for x in range(M):
+            if graph[y][x] == 1 and not visit[y][x]:
+                BFS(x, y, graph, visit, N, M)
+                worm_count += 1
+    
+    print(worm_count)
