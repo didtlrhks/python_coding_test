0a2fc609e842e4560bacf6aae3662ee7a1061c25
sikwan <yysskk99999@naver.com>
2025-12-11 19:26:16 +0900

    err code add

--- 1012.py
+++ 1012.py
@@ -9,52 +9,52 @@ result = []
 
 
 
-for i in range(T):
+for _ in range(T):
     M,N,K = map(int,input().split())
-    for j in range(K):
+    visit = [[False] * M for _ in range(N)]
+    graph = [[0] * M for _ in range(N)]
+    for _ in range(K):
         X,Y = map(int,input().split())
+        graph[Y][X] = 1
+        
+    result = 0
+      
         
         
-visit = [[False] * M for _ in range(N)]
-graph = list([[] * M for _ in range(N)])
+   
 
-def BFS(x,y):
-    queue = deque()
-    queue.append((x,y))
-    visit[x][y] = True
-    count = 0
-    
-    while queue:
-        queue.popleft()
+    def BFS(x,y):
+        queue = deque()
         queue.append((x,y))
+        visit[y][x] = True
+        count = 0
         
-        for i in range(4):
-            nx = x + L[i]
-            ny = y + H[i]
-        
-        
-        if 0 <= nx < M and 0 <= ny < N:
-            if not visit[nx][ny] and graph[nx][ny] == '1':
-                queue.append((nx,ny))
-                visit = True
-                count += 1
+        while queue:
+            x,y = queue.popleft()
+            
+            
+            for i in range(4):
+                nx = x + L[i]
+                ny = y + H[i]
+            
+            
+                if 0 <= nx < M and 0 <= ny < N:
+                    if not visit[nx][ny] and graph[nx][ny] == 1:
+                        queue.append((nx,ny))
+                        visit[nx][ny] = True
                 
-    return count
+                    
+        return 1
 
 
-for i in range(N):
-    for j in range(M):
-        if 0 <= i < M and 0 <= j < N:
-            if not visit[i][j] and graph[i][j] == '1':
-                result.append((BFS(i,j)))
-                visit = True
-                
-                
-for i in result:
-    print(len(i))
-                
-                        
-                 
+    for i in range(N):
+        for j in range(M):
+            if 0 <= i < M and 0 <= j < N:
+                if not visit[i][j] and graph[i][j] == 1:
+                    result.append((BFS(i,j)))
+                    visit = True
+                    []
+
                         
             
             
