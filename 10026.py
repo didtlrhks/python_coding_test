dbeb64b9ef2972f8682681ddeb2c5c5eab95b26c
sikwan <yysskk99999@naver.com>
2025-12-15 15:21:34 +0900

    10026 add

--- 10026.py
+++ 10026.py
@@ -3,31 +3,24 @@ from collections import deque
 
 
 N = int(input())
-graph = list([] for _ in range(N))# 문법을 모르겠음 아마 문자열을 받는거면 될거같은데.. 
-visit = [False] * N
-count = 1
+graph = [list(input().strip()) for _ in range(N)]
+
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 
-for i in range(N):
-    for j in range(N):
-        graph[i][j] = # 여기에다가 문자열을 넣어야 되고 
-    
-    
 
 def BFS(x,y):
     queue = deque()
     queue.append((x,y))
     visit[x][y] = True
+    color = graph[x][y]
     
 
     
     while queue:
         x,y = queue.popleft()      
-        queue.append((x,y))
-        
         
         for i in range(4):
         nx = x + dx[i]
@@ -35,11 +28,12 @@ def BFS(x,y):
         
         
         if 0 <= nx < N and 0 <= ny < N:
-            if not visit[nx][ny] and graph[nx][ny] == "1":
+            if not visit[nx][ny] and graph[nx][ny] == color:
                 visit[nx][ny] == True
-                count += 1
+                queue.appned((nx,ny))
                 
-return count 
+                
+visit
 
         
         
