7da8200705acfed1eae08b1b14cdcfdd479aeaf9
sikwan <yysskk99999@naver.com>
2025-12-16 13:15:36 +0900

    2468 bfs logic add

--- 2468.py
+++ 2468.py
@@ -6,23 +6,39 @@ from collections import deque
 N = int(input())
 graph = [list(map(int,input().split())) for _ in range(N)]
 visit = [[False] * N for _ in range(N)]
+count = 0
 
-
-x = [-1,1,0,0]
-y = [0,0,-1,1]
-
-
-
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
 
 
-def BFS(x,y,graph,visit):
+
+
+
+def BFS(x,y,h,graph,visit):
     queue = deque()
     queue.append((x,y))
     visit[x][y] = True
     
     while queue:
         x,y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0<= nx < N and 0 <= ny < N:
+                if not visit[nx][ny] and graph[nx][ny] > h :
+                    visit[nx][ny] = True
+                    queue.append((nx,ny))
+                    count += 1
+                    
+
+                    
+            
+        
+        
         
         
         
