cdb1fbe4129688f7ea307f4ebecf3d3b5d2bdf54
sikwan <yysskk99999@naver.com>
2025-12-18 15:27:42 +0900

    4963 add

--- 4963.py
+++ 4963.py
@@ -3,36 +3,43 @@ from collections import deque
 
 
 
-W,H = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(H)]
-count = 0
-
-dx = [-1,1,-1,0,0,1,1,1]
-dy = [-1,0,1,-1,1,-1,0,1]
+dw = [-1,1,-1,0,0,1,1,1]
+dh = [-1,0,1,-1,1,-1,0,1]
 
 def BFS(w,h,graph,visit):
     queue = deque()
     queue.append((w,h))
     
-    visit[w][h] = True
+    visit[h][w] = True
     
     while queue:
         w,h = queue.popleft()
         
         for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nw = w + dw[i]
+            nh = h + dh[i]
         
-            if 0 <= nx < N and 0<= ny < N:
-                if not visit[nx][ny] and graph[nx][ny] == False:
-                    visit[nx][ny] == True
-                    queue.append((nx,ny))
+            if 0 <= nw < W and 0<= nh < H:
+                if not visit[nh][nw] and graph[nh][nw] == 1:
+                    visit[nh][nw] = True
+                    queue.append((nw,nh))
                     count += 1
-    return count
-
-           
 
 
-        
-        
+while True:
     
+
+    W,H = map(int,input().split())
+    if W == 0 and H == 0:
+        break
+    graph = [list(map(int,input().split())) for _ in range(H)]
+    visit = [[False]*W for _ in range(H)]
+    count = 0
+    
+    for h in range(H):
+        for w in range(W):
+            if graph[h][w] == 1 and not visit[h][w]:
+                BFS(w,h,graph,visit,W,H)
+                count+=1
+                
+    print(count)
