f6881e1eabd952288253772c0444de58ece8e3b3
sikwan <yysskk99999@naver.com>
2025-12-22 17:55:03 +0900

    1987 add

--- 1987.py
+++ 1987.py
@@ -4,8 +4,26 @@ input = sys.stdin.readline
 
 
 R,C = map(int,input().split())
-graph = [list(input().strip()) for _ in range(R)]
+board = [list(input().strip()) for _ in range(R)]
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-#test 1987
+
+used = [False] * 26
+answer = 0
+
+
+
+def dfs(x,y,cnt):
+    global answer
+    answer = max(answer,cnt)
+    
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        
+        if 0 <= nx < R and 0 <= ny < C:
+            idx = ord(board[nx][ny]) - ord('A')
+            
+        
+        
