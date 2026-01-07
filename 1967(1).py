0aaaa61bf0fe3ca5d99b96e2368028ae7e4b070a
sikwan <yysskk99999@naver.com>
2026-01-07 11:55:07 +0900

    1967 add

--- 1967(1).py
+++ 1967(1).py
@@ -8,33 +8,60 @@ n = int(input())
 
 tree = [[] for _ in range(n+1)]
 
+
 for i in range(n-1):
     parent,child,weight = map(int,input().split())
     tree[parent].append((child,weight))
     tree[child].append((parent,weight))
     
-
 visit = [False] * (n+1)
 max_tree = 0
-farthest_node = 1
+max_node = 1
+
+
 
 def DFS(node,tree):
-    global max_tree, farthest_node
+    global max_tree,max_node
     visit[node] = True
     
     if tree > max_tree:
         max_tree = tree
-        farthest_node = node   
-    
+        max_node = node
+        
+        
     for next_node,weight in tree[node]:
         if not visit[next_node]:
-            dfs(next_node,tree + weight)
+            DFS(next_node,weight + tree)
             
 DFS(1,0)
 
 visit = [False] * (n+1)
 max_tree = 0
 
-DFS(farthest_node,0)
+DFS(max_node,0)
 
 print(max_tree)
+
+            
+
+    
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
